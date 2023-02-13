d2ad877f9451ff36679c44327e84cacb9cf7077e
Gergo Szucs <gergobenceszucs@gmail.com>
2023-02-13 15:58:55 +0100

    fix some issues

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { SecretService } from './secret.service';
+
 @Injectable({
     providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-    constructor(private authService: AuthService, private router: Router) { };
+    isLoggedIn: boolean;
+
+    constructor(private authService: AuthService, private router: Router, private secretService: SecretService) {
+
+    };
+
     canActivate(
         next: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot): boolean {
-        let isLoggedIn = this.authService.isAuthenticated();
-        if (isLoggedIn) {
-            return true
-        } else {
-            this.router.navigate(['/admin']);
-        }
+        state: RouterStateSnapshot) {
+        return this.secretService.getPwd().then((doc) => {
+            const secret = (doc.data() as any).password as string;
+
+            if (atob(localStorage.getItem("stingerPwd")) === secret) {
+                this.isLoggedIn = true;
+            }
+
+            if (this.isLoggedIn) {
+                return true
+            } else {
+                this.router.navigate(['/admin']);
+            }
+        }).catch(() => { return false; });
     }
 }
